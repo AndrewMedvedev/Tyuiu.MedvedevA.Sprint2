14b51c77bef440dd4faf59514dec3a787097f370
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-10-14 20:48:33 +0500

    Task5

---
+++ Tyuiu.MedvedevA.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MedvedevA.Sprint2.Task5.V1.Lib;
+
+namespace Tyuiu.MedvedevA.Sprint2.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int value;
+
+            Console.WriteLine("Введите месяц: ");
+            value = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Резултьтат: " + ds.FindMonthDaysCount(value));
+            Console.ReadKey();
+        }
+    }
+}
