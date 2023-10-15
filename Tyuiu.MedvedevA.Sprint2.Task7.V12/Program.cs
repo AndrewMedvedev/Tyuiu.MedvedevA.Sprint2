819190f81f17c3305e364274094684bb8f7671eb
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-10-15 20:30:52 +0500

    Task7

---
+++ Tyuiu.MedvedevA.Sprint2.Task7.V12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MedvedevA.Sprint2.Task7.V12.Lib;
+
+namespace Tyuiu.MedvedevA.Sprint2.Task7.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+           
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            if (res == true)
+            {
+                Console.WriteLine("Точка входит в указанную область!");
+            }
+            else
+            {
+                Console.WriteLine("Точка НЕ входит в указанную область!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
